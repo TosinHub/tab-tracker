3d33825e70213e9bcfcaf898ca4b61b7adfa8d2c
Tosin <tosinolugbenga@gmail.com>
2017-10-19 14:52:12 +0100

    log out implemented

--- client/src/components/header.vue
+++ client/src/components/header.vue
@@ -3,16 +3,26 @@
    <v-toolbar-title class="mr-4" ><span @click="navigateTo({name: 'home'})" class="home">Tab Tracker</span> </v-toolbar-title>
 
  <v-toolbar-items>
-   <v-btn flat dark>Browse</v-btn>
+   <v-btn   flat dark 
+ @click="navigateTo({name: 'songs'})"
+   >Browse</v-btn>
  </v-toolbar-items>
  <v-spacer></v-spacer>
  <v-toolbar-items>
    
-    <v-btn flat dark 
+    <v-btn
+    v-if="!$store.state.isUserLoggedIn" flat dark 
  @click="navigateTo({name: 'login'})"
  >Login</v-btn>
 
-    <v-btn flat dark 
+   <v-btn
+    v-if="$store.state.isUserLoggedIn" flat dark 
+ @click="logout"
+ >Logout</v-btn>
+
+    <v-btn
+    v-if="!$store.state.isUserLoggedIn"
+     flat dark 
  @click="navigateTo({name: 'register'})"
  >Sign Up</v-btn>
  
@@ -25,6 +35,13 @@ export default{
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'home'
+      })
     }
   }
 }
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Register from '@/components/Register'
 import Login from '@/components/Login'
+import Songs from '@/components/Songs'
+
 Vue.use(Router)
 
 export default new Router({
@@ -21,6 +23,11 @@ export default new Router({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/songs',
+      name: 'songs',
+      component: Songs
     }
   ]
 })
